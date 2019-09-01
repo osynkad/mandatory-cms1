db3bad076c101358f7873b26ff20dbf05f37865e
ultraemme <e.larsson1994@gmail.com>
2019-09-02 00:55:31 +0200

    docker storage

---
+++ storage/collections/articles.collection.php
@@ -0,0 +1,109 @@
+<?php
+ return array (
+  'name' => 'articles',
+  'label' => '',
+  '_id' => 'articles5d6ad7619e0e8',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'title',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'markdown',
+      'label' => '',
+      'type' => 'markdown',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    2 => 
+    array (
+      'name' => 'published_on',
+      'label' => '',
+      'type' => 'date',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    3 => 
+    array (
+      'name' => 'author',
+      'label' => '',
+      'type' => 'collectionlink',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'sortable' => false,
+  'in_menu' => false,
+  '_created' => 1567283041,
+  '_modified' => 1567283130,
+  'color' => '',
+  'acl' => 
+  array (
+  ),
+  'rules' => 
+  array (
+    'create' => 
+    array (
+      'enabled' => false,
+    ),
+    'read' => 
+    array (
+      'enabled' => false,
+    ),
+    'update' => 
+    array (
+      'enabled' => false,
+    ),
+    'delete' => 
+    array (
+      'enabled' => false,
+    ),
+  ),
+  'icon' => 'heading.svg',
+);
